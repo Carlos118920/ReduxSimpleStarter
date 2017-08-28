949ff2d71903a97209f8fff3a9c649806bafde10
Your Name <you@example.com>
2017-08-28 14:13:07 -0700

    section 1 lecture 17

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react"
+import ReactDOM from "react-dom"
+import SearchBar from "./components/search_bar"
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "AIzaSyCEQrlQjJYOqrc4ID5EB4FBZR3A7v-E1EY";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.getElementById("app"))
